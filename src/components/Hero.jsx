34ae185be6a5c17b0a26cc0377e2e6ad12f4c32d
ilariamotta <ilaria.motta93@gmail.com>
2026-05-21 11:39:46 +0200

    fix: hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
-    return (<>
+  return (
     <div className="hero-wrapper mt-5">
-    <img src="/hero.png" alt="Hero" className="img-fluid hero-pixel"/>
+      <div className="hero-box">
+        <img src="/hero.png" alt="Hero" className="img-fluid hero-pixel" />
+        <div className="hero-buttons d-none d-lg-flex">
+          <Link to="/chi-siamo" className="hero-btn">About</Link>
+          <Link to="/autori" className="hero-btn">Autori</Link>
+          <Link to="/categorie" className="hero-btn">Categorie</Link>
+        </div>
+      </div>
     </div>
-    </>
-    )
+  );
 }
